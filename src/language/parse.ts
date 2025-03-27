f8aec1b81b448a95b9d62e8fda03a8532e8cc905
dylan.lee <dylan.lee@daangn.com>
2025-03-27 22:55:16 +0900

    Implement parse for directly related user type

--- src/language/parse.ts
+++ src/language/parse.ts
@@ -1,19 +1,52 @@
 import { transformer } from "@openfga/syntax-transformer";
 import type { BaseEntityTypeMap } from "BaseEntityTypeMap";
-import type { GraplixSchema } from "../GraplixSchema";
+import type {
+  GraplixSchema,
+  GraplixSchemaRelationDefinition,
+} from "../GraplixSchema";
+import type { ValidatedRelationMetadata } from "./ValidatedModel";
 import { validate } from "./validate";
+
+type GraplixSchemaEntry<T extends BaseEntityTypeMap> = [
+  string,
+  { [relationName: string]: GraplixSchemaRelationDefinition<T> },
+];
 
 export function parse<T extends BaseEntityTypeMap>(
   input: string,
 ): GraplixSchema<T> {
-  const graplixSchema: Record<string, {}> = {};
+  const schemaEntries: GraplixSchemaEntry<T>[] = [];
   const ast = transformer.transformDSLToJSONObject(input);
 
   validate(ast);
 
   for (const typeDefinition of ast.type_definitions) {
-    graplixSchema[typeDefinition.type] = {};
+    const typeDef: {
+      [relationName: string]: GraplixSchemaRelationDefinition<T>;
+    } = {};
+
+    for (const [relationName, relation] of Object.entries(
+      typeDefinition.metadata?.relations ?? {},
+    )) {
+      const relationDef = getDirectlyRelatedUserTypes<T>(relation);
+      if (relationDef) {
+        typeDef[relationName] = relationDef;
+      }
+    }
+
+    schemaEntries.push([typeDefinition.type, typeDef]);
   }
 
-  return graplixSchema as GraplixSchema<T>;
+  return Object.fromEntries(schemaEntries) as GraplixSchema<T>;
+}
+
+function getDirectlyRelatedUserTypes<T extends BaseEntityTypeMap>(
+  relation: ValidatedRelationMetadata,
+): GraplixSchemaRelationDefinition<T> | undefined {
+  const relationDef = relation.directly_related_user_types?.[0];
+  if (!relationDef) return undefined;
+
+  return {
+    type: relationDef.type as Extract<keyof T, string>,
+  } satisfies GraplixSchemaRelationDefinition<T>;
 }
