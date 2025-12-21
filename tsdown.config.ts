2ff434e780a1c25db1398ce71c3890545e386fff
Tony Won <tony@daangn.com>
2025-12-21 17:13:24 +0900

    fix typescript

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,10 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: "./src/index.ts",
   outDir: "./dist",
-  dts: {
-    transformer: "typescript",
-    autoAddExts: true,
-  },
   format: ["esm", "cjs"],
   clean: true,
+  dts: true,
 });
